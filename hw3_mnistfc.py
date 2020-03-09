cd8474f61c165c61f185948843df1eafd8967d7f
Bryan Baker <bbaker1229@gmail.com>
2020-03-08 23:08:12 -0500

    Updated information on the mnist neural network file.

--- hw3_mnistfc.py
+++ hw3_mnistfc.py
@@ -7,3 +7,68 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torchvision
 import torchvision.transforms as transforms
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(28 * 28, 128, bias=True)
+        self.rlu = nn.ReLU()
+        self.fc2 = nn.Linear(128, 10, bias=False)
+        self.soft = nn.Softmax()
+
+    def forward(self, x):
+        x = x.view(-1, 28 * 28)
+        x = F.relu(self.fc1(x))
+        x = self.soft(self.fc2(x))
+        return x
+
+
+def get_mnist_data():
+    transform = transforms.Compose(
+        [transforms.ToTensor(),
+         transforms.Normalize((0.5, ), (0.5, ))])
+    train = torchvision.datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+    test = torchvision.datasets.MNIST(root='./data', train=False, download=True, transform=transform)
+    return train, test
+
+
+def load_mnist_data(train, test):
+    train_load = torch.utils.data.DataLoader(train, batch_size=32, shuffle=True, num_workers=0)
+    test_load = torch.utils.data.DataLoader(test, batch_size=32, shuffle=False, num_workers=0)
+    return train_load, test_load
+
+
+train_set, test_set = get_mnist_data()
+train_loader, test_loader = load_mnist_data(train_set, test_set)
+
+net = Net()
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001)
+
+for epoch in range(2):
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parmeter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print("Finished Training")
+
+# Fix the softmax error....
